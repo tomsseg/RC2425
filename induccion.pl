4e67e9e83650308c21d1da0e09d25c5c7484c8d5
tomsseg <tomasestradag04@gmail.com>
2025-03-06 09:53:50 +0100

    Create induccion.pl

---
+++ induccion.pl
@@ -0,0 +1,10 @@
+% Inducción Matemática
+% Quiero probar si el programa funciona con todos los elementos de un conjunto
+% Los elementos del conjunto tienen que ser ordenables
+% Existe un elemento más pequeño y para ese lel programa tiene que funcionar
+% Si funciona para un elemento, funciona para el elemento inmediatamente mayor
+
+% Listas
+n0=[], [_]<[_, _]?
+
+% Aplicable a árboles y grafos
