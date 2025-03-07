d78a467e0a058c1d2bf25c10f21b89132eb134fa
tomsseg <tomasestradag04@gmail.com>
2025-03-07 13:49:07 +0100

    Update tipo_triangulo.pl

--- excercism/tipo_triangulo.pl
+++ excercism/tipo_triangulo.pl
@@ -1,5 +1,4 @@
-triangulo (A,B,C,tipo).
-ladovalido (A, B, C) :- A > 0, B > 0, C > 0, A + B > C, A + C > B, B + C > A.
+ladovalido (A, B, C) :- A > 0, B > 0, C > 0, A + B >= C, A + C >= B, B + C >= A.
 triangulo (A, B, C, equilatero) :- ladovalido(A, B, C), A == B, B == C.
 triangulo (A, B, C, isosceles) :- ladovalido(A, B, C), A == B.
 triangulo (A, B, C, isosceles) :- ladovalido(A, B, C), C == B.
