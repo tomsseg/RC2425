d82e44fee81abf4ed07707bf29834b251f6b2e4f
tomsseg <tomasestradag04@gmail.com>
2025-03-07 14:16:01 +0100

    Update tipo_triangulo.pl

--- excercism/tipo_triangulo.pl
+++ excercism/tipo_triangulo.pl
@@ -1,6 +1,8 @@
-ladovalido (A, B, C) :- A > 0, B > 0, C > 0, A + B >= C, A + C >= B, B + C >= A.
-triangulo (A, B, C, equilatero) :- ladovalido(A, B, C), A == B, B == C.
-triangulo (A, B, C, isosceles) :- ladovalido(A, B, C), A == B.
-triangulo (A, B, C, isosceles) :- ladovalido(A, B, C), C == B.
-triangulo (A, B, C, isosceles) :- ladovalido(A, B, C), A == C.
-triangulo (A, B, C, escaleno) :- ladovalido(A, B, C), A \= B, A \= C, B \= C.
+tvalido(A, B, C) :- A > 0, B > 0, C > 0, A + B >= C, A + C >= B, B + C >= A.
+es(A, B, C) :- tvalido(A, B, C), A =:= B, B =:= C.
+i(A, B, C) :- ladovalido(A, B, C), A =:= B.
+i(A, B, C) :- ladovalido(A, B, C), C =:= B.
+i(A, B, C) :- ladovalido(A, B, C), A =:= C.
+eq(A, B, C) :- ladovalido(A, B, C), A \= B, A \= C, B \= C.
+
+triangulo(A,B,C,R) :- la
