41dfbe1e735b2933c9897d48cb92e260f2cd4bcf
tomsseg <tomasestradag04@gmail.com>
2025-03-07 14:30:20 +0100

    Update tipo_triangulo.pl

--- excercism/tipo_triangulo.pl
+++ excercism/tipo_triangulo.pl
@@ -1,8 +1,7 @@
-tvalido(A, B, C) :- A > 0, B > 0, C > 0, A + B >= C, A + C >= B, B + C >= A.
-es(A, B, C) :- tvalido(A, B, C), A =:= B, B =:= C.
-i(A, B, C) :- ladovalido(A, B, C), A =:= B.
-i(A, B, C) :- ladovalido(A, B, C), C =:= B.
-i(A, B, C) :- ladovalido(A, B, C), A =:= C.
-eq(A, B, C) :- ladovalido(A, B, C), A \= B, A \= C, B \= C.
+tvalido(A, B, C) :- A > 0, B > 0, C > 0, X is A + B, X >= C, Y is A + C ,Y >= B,Z is B + C, Z >= A.
 
-triangulo(A,B,C,R) :- la
+triangulo(A, B, C,"equilatero") :- A = B, C = B.
+triangulo(A, B, C,"isosceles") :- tvalido(A, B, C), A = B.
+triangulo(A, B, C,"isosceles") :- tvalido(A, B, C), C = B.
+triangulo(A, B, C,"isosceles") :- tvalido(A, B, C), A = C.
+triangulo(A, B, C,"escaleno") :- tvalido(A, B, C), A \= B, A \= C, B \= C.
