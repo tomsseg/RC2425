6714359af1d68e8c4ea32f987356be601e0e512d
tomsseg <tomasestradag04@gmail.com>
2025-04-25 13:41:06 +0200

    Update wedding_woes.pl

--- excercism/wedding_woes.pl
+++ excercism/wedding_woes.pl
@@ -5,9 +5,8 @@ likes(esteban, malena).
 likes(malena, esteban).
 likes(gustavo, valeria).
 
-pairing(A, B) :- chatty(A), A\=B.% una persona habladora se empareja con cualquiera
-pairing(B, A) :- chatty(A), A\=B.
-pairing(A, B) :- likes(A, B) , likes(B, A). % a y b se gustan y se emparejan
+pairing(First, Second) :- likes(First, Second), likes(Second, First).
+pairing(First, Second) :- chatty(First); chatty(Second).
 
 seating(A, B, C, D, E) :- pairing(A, B), pairing(B, C), pairing(C, D), pairing(D, E), pairing(E, A).
 
