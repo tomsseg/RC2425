4b5fde2bc88258578660f64e9c60096df5a1e3cf
tomsseg <tomasestradag04@gmail.com>
2025-05-02 10:24:57 +0200

    Update and rename tipo_triangulo.pl to triangle.pl

--- excercism/tipo_triangulo.pl
+++
@@ -1,7 +0,0 @@
-tvalido(A, B, C) :- A > 0, B > 0, C > 0, X is A + B, X >= C, Y is A + C ,Y >= B,Z is B + C, Z >= A.
-
-triangulo(A, B, C,"equilatero") :- A = B, C = B.
-triangulo(A, B, C,"isosceles") :- tvalido(A, B, C), A = B.
-triangulo(A, B, C,"isosceles") :- tvalido(A, B, C), C = B.
-triangulo(A, B, C,"isosceles") :- tvalido(A, B, C), A = C.
-triangulo(A, B, C,"escaleno") :- tvalido(A, B, C), A \= B, A \= C, B \= C.
---
+++ excercism/triangle.pl
@@ -0,0 +1,28 @@
+triangle(Side1, Side2, Side3, "equilateral") :-
+      Side1 =:= Side2,
+      Side2 =:= Side3,
+      valid_triangle(Side1, Side2, Side3).
+
+triangle(Side1, Side2, Side3, "isosceles") :-
+    (
+	    Side1 =:= Side2
+    ;	Side1 =:= Side3
+    ;	Side2 =:= Side3
+    ),
+    valid_triangle(Side1, Side2, Side3).
+
+triangle(Side1, Side2, Side3, "scalene") :-
+    \+ triangle(Side1, Side2, Side3, "equilateral"), % /+ no es cierto que
+    \+ triangle(Side1, Side2, Side3, "isosceles"),
+    valid_triangle(Side1, Side2, Side3).
+
+valid_triangle(Side1, Side2, Side3) :-
+    Add23 is Side2 + Side3,
+    Add12 is Side1 + Side2,
+    Add13 is Side1 + Side3,
+    not(Side1 > Add23),
+    not(Side2 > Add13),
+    not(Side3 > Add12),
+    Side1 \== 0,
+    Side2 \== 0,
+    Side3 \== 0.
