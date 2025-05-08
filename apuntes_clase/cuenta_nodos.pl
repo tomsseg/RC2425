8a59ef3e7c9658af9e81677f150987e1ed9391e9
tomsseg <tomasestradag04@gmail.com>
2025-05-08 08:56:34 +0200

    Update cuenta_nodos.pl

--- apuntes_clase/cuenta_nodos.pl
+++ apuntes_clase/cuenta_nodos.pl
@@ -1,3 +1,10 @@
+% Para probar el código:
+
+a(1,
+  a(2,a(4,nil,nil),a(5,nil,nil)),
+  a(3,a(6,nil,nil),a(7,nil,nil))).
+
+% Cuenta nodos:
 cuenta_nodos(arbol_vacio,0).
 cuenta_nodos(a(_,AI,AD),R),:-
   cuenta_nodos(AI,RI),
